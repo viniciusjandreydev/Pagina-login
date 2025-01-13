3b7857e6cce0c0b9ad0082986f01b3a5d8d0a19a
Vinícius Jandrey <viniciusctts@gmail.com>
2025-01-13 15:45:38 -0300

    Atualização final

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'meusqlsite.c7oewsgak7wr.sa-east-1.rds.amazonaws.com'; // Endereço do servidor MySQL (use 'localhost' se estiver rodando localmente)
-$db = 'sistema-login'; // Nome do banco de dados
-$user = 'admin'; // Nome de usuário do MySQL
+$host = 'sql106.byethost10.com'; // Endereço do servidor MySQL (use 'localhost' se estiver rodando localmente)
+$db = 'b10_38098931_sistemalogin'; // Nome do banco de dados
+$user = 'b10_38098931'; // Nome de usuário do MySQL
 $pass = 'ViniciusJH12'; // Senha do MySQL (deixe em branco se não houver senha)
 
 try {
